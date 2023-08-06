2c25f9ce5a4af326309ff97433ac9fb3faa1a060
thayna-oliveira <oliveira.thayna@outlook.com>
2023-08-06 12:31:24 +0100

    add page wrapper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { Sidebar } from './shared/components/sidebar/sidebar.type';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  sidebar: Sidebar = {
+    logo: 'logo',
+    itens: [
+      { icon: 'home', description: 'Home' },
+      { icon: 'hashtag', description: 'Explore' },
+      { icon: 'notification', description: 'Notifications' },
+      { icon: 'message', description: 'Messages' },
+      { icon: 'bookmark', description: 'Bookmarks' },
+      { icon: 'more', description: 'More' },
+    ],
+  };
+}
